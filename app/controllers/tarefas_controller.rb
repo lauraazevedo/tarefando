03930b80520c7d53dee1b337b6d341c3ec1b3cd7
laura <laure_nha@hotmail.com>
2016-12-10 16:59:08 -0200

    finalizamos cadastro de tarefas

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -7,4 +7,12 @@ class TarefasController < ApplicationController
   	@funcionario = Funcionario.find(params[:funcionario_id])
     @tarefa = Tarefa.new
   end
+  def create
+  	@funcionario = Funcionario.find(params[:funcionario_id])
+  	tarefa_params = params.require(:tarefa).permit(:titulo, :descricao, :prazo, :status)
+    @funcionario.tarefas.create(tarefa_params)
+    
+    flash[:success] = "TAREFA CRIADA COM SUCESSO"
+    redirect_to funcionario_tarefas_url(@funcionario)
+end
 end
