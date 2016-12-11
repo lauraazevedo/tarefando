7e6cd8685f88b144fc5a0fc539c3eaa7298ed02e
Jessica <jlzimmerhansl@gmail.com>
2016-12-10 17:17:20 -0800

    botão de fluxo

--- app/controllers/tarefas_controller.rb
+++ app/controllers/tarefas_controller.rb
@@ -9,10 +9,27 @@ class TarefasController < ApplicationController
   end
   def create
   	@funcionario = Funcionario.find(params[:funcionario_id])
-  	tarefa_params = params.require(:tarefa).permit(:titulo, :descricao, :prazo, :status)
-    @funcionario.tarefas.create(tarefa_params)
-    
+  	tarefa_params = params.require(:tarefa).permit(:titulo, :descricao, :prazo)
+
+    @funcionario.tarefas.create(tarefa_params.merge(status: 'pendente'))
+
     flash[:success] = "TAREFA CRIADA COM SUCESSO"
     redirect_to funcionario_tarefas_url(@funcionario)
 end
+
+  def update
+    @funcionario = Funcionario.find(params[:funcionario_id])
+    @tarefa = Tarefa.find(params[:id])
+
+    if @tarefa.status == "pendente"
+      novo_status = "iniciada"
+    elsif @tarefa.status == "iniciada"
+      novo_status = "completa"
+    end
+
+    @tarefa.status = novo_status
+    @tarefa.save
+
+    redirect_to funcionario_tarefas_url(@funcionario)
+  end
 end
